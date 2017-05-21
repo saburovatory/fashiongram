033dd90733aee819a7a516327098ce6118cd62d6
oceandrama <oceandrama@gmail.com>
2017-05-21 18:42:33 +0300

    Initial commit

---
+++ app/src/main/java/io/github/froger/instamaterial/ui/activity/PublishActivity.java
@@ -0,0 +1,265 @@
+package io.github.froger.instamaterial.ui.activity;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewTreeObserver;
+import android.view.animation.OvershootInterpolator;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import butterknife.BindView;
+import butterknife.OnCheckedChanged;
+import io.github.froger.instamaterial.R;
+import io.github.froger.instamaterial.Utils;
+
+public class PublishActivity extends BaseActivity {
+    public static final String ARG_TAKEN_PHOTO_URI = "arg_taken_photo_uri";
+
+    @BindView(R.id.tbFollowers)
+    ToggleButton tbFollowers;
+    @BindView(R.id.tbDirect)
+    ToggleButton tbDirect;
+    @BindView(R.id.ivPhoto)
+    ImageView ivPhoto;
+    @BindView(R.id.etDescription)
+    EditText etDescription;
+
+    private boolean propagatingToggleState = false;
+    private Uri photoUri;
+    private int photoSize;
+
+    public static void openWithPhotoUri(Activity openingActivity, Uri photoUri) {
+        Intent intent = new Intent(openingActivity, PublishActivity.class);
+        intent.putExtra(ARG_TAKEN_PHOTO_URI, photoUri);
+        openingActivity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_grey600_24dp);
+        photoSize = getResources().getDimensionPixelSize(R.dimen.publish_photo_thumbnail_size);
+
+        if (savedInstanceState == null) {
+            photoUri = getIntent().getParcelableExtra(ARG_TAKEN_PHOTO_URI);
+        } else {
+            photoUri = savedInstanceState.getParcelable(ARG_TAKEN_PHOTO_URI);
+        }
+        updateStatusBarColor();
+
+        ivPhoto.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                ivPhoto.getViewTreeObserver().removeOnPreDrawListener(this);
+                loadThumbnailPhoto();
+                return true;
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void updateStatusBarColor() {
+        if (Utils.isAndroid5()) {
+            getWindow().setStatusBarColor(0xff888888);
+        }
+    }
+
+    private void loadThumbnailPhoto() {
+        ivPhoto.setScaleX(0);
+        ivPhoto.setScaleY(0);
+        Picasso.with(this)
+                .load(photoUri)
+                .centerCrop()
+                .resize(photoSize, photoSize)
+                .into(ivPhoto, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        ivPhoto.animate()
+                                .scaleX(1.f).scaleY(1.f)
+                                .setInterpolator(new OvershootInterpolator())
+                                .setDuration(400)
+                                .setStartDelay(200)
+                                .start();
+                    }
+
+                    @Override
+                    public void onError() {
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_publish, menu);
+        return true;
+    }
+
+    public int uploadFile(String sourceFileUri) {
+        String fileName = sourceFileUri;
+        int serverResponseCode = 0;
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+
+        if (sourceFile.isFile()) {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL("http://u0306965.plsk.regruhosting.ru/uploadFile.php");
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + fileName + "\"" + lineEnd);
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+                try {
+                    url = new URL("http://u0306965.plsk.regruhosting.ru/" +
+                            "chat.php?action=insert" +
+                            "&author=Ruslan" +
+                            "&text=" + etDescription.getText().toString().replace(' ', '+') +
+                            "&img=/images/" + photoUri.getLastPathSegment());
+
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setConnectTimeout(10000); // ждем 10сек
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+                    conn.connect();
+
+                    serverResponseCode = conn.getResponseCode();
+
+                } catch (Exception e) {
+                    Log.i("chat", "+ BackgroundService ошибка: " + e.getMessage());
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        } // End else block
+
+        return serverResponseCode;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_publish) {
+
+            new Thread(new Runnable() {
+                public void run() {
+                    uploadFile(photoUri.getPath());
+                }
+            }).start();
+
+            bringMainActivityToTop();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void bringMainActivityToTop() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.setAction(MainActivity.ACTION_SHOW_LOADING_ITEM);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(ARG_TAKEN_PHOTO_URI, photoUri);
+    }
+
+    @OnCheckedChanged(R.id.tbFollowers)
+    public void onFollowersCheckedChange(boolean checked) {
+        if (!propagatingToggleState) {
+            propagatingToggleState = true;
+            tbDirect.setChecked(!checked);
+            propagatingToggleState = false;
+        }
+    }
+
+    @OnCheckedChanged(R.id.tbDirect)
+    public void onDirectCheckedChange(boolean checked) {
+        if (!propagatingToggleState) {
+            propagatingToggleState = true;
+            tbFollowers.setChecked(!checked);
+            propagatingToggleState = false;
+        }
+    }
+}
---
+++ app/src/main/java/io/github/froger/instamaterial/ui/adapter/FeedAdapter.java
@@ -0,0 +1,336 @@
+package io.github.froger.instamaterial.ui.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.froger.instamaterial.R;
+import io.github.froger.instamaterial.ui.activity.MainActivity;
+import io.github.froger.instamaterial.ui.view.LoadingFeedItemView;
+
+public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static final String ACTION_LIKE_BUTTON_CLICKED = "action_like_button_button";
+    public static final String ACTION_LIKE_IMAGE_CLICKED = "action_like_image_button";
+
+    public static final int VIEW_TYPE_DEFAULT = 1;
+    public static final int VIEW_TYPE_LOADER = 2;
+
+    private final List<FeedItem> feedItems = new ArrayList<>();
+
+    private Context context;
+    private SQLiteDatabase chatDBlocal;
+    private OnFeedItemClickListener onFeedItemClickListener;
+
+    private boolean showLoadingView = false;
+
+    public FeedAdapter(Context context, SQLiteDatabase db) {
+        this.context = context;
+        this.chatDBlocal = db;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_DEFAULT) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_feed, parent, false);
+            CellFeedViewHolder cellFeedViewHolder = new CellFeedViewHolder(view);
+            setupClickableViews(view, cellFeedViewHolder);
+            return cellFeedViewHolder;
+        } else if (viewType == VIEW_TYPE_LOADER) {
+            LoadingFeedItemView view = new LoadingFeedItemView(context);
+            view.setLayoutParams(new LinearLayoutCompat.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+            );
+            return new LoadingCellFeedViewHolder(view);
+        }
+
+        return null;
+    }
+
+    private void setupClickableViews(final View view, final CellFeedViewHolder cellFeedViewHolder) {
+        cellFeedViewHolder.btnComments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onFeedItemClickListener.onCommentsClick(view, cellFeedViewHolder.getAdapterPosition());
+            }
+        });
+        cellFeedViewHolder.btnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onFeedItemClickListener.onMoreClick(v, cellFeedViewHolder.getAdapterPosition());
+            }
+        });
+        cellFeedViewHolder.ivFeedCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = cellFeedViewHolder.getAdapterPosition();
+                feedItems.get(adapterPosition).likesCount++;
+                notifyItemChanged(adapterPosition, ACTION_LIKE_IMAGE_CLICKED);
+                if (context instanceof MainActivity) {
+                    ((MainActivity) context).showLikedSnackbar();
+                }
+            }
+        });
+        cellFeedViewHolder.btnLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = cellFeedViewHolder.getAdapterPosition();
+                feedItems.get(adapterPosition).likesCount++;
+                notifyItemChanged(adapterPosition, ACTION_LIKE_BUTTON_CLICKED);
+                if (context instanceof MainActivity) {
+                    ((MainActivity) context).showLikedSnackbar();
+                }
+            }
+        });
+        cellFeedViewHolder.ivUserProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onFeedItemClickListener.onProfileClick(view);
+            }
+        });
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        ((CellFeedViewHolder) viewHolder).bindView(feedItems.get(position));
+
+        if (getItemViewType(position) == VIEW_TYPE_LOADER) {
+            bindLoadingFeedItem((LoadingCellFeedViewHolder) viewHolder);
+        }
+    }
+
+    private void bindLoadingFeedItem(final LoadingCellFeedViewHolder holder) {
+        holder.loadingFeedItemView.setOnLoadingFinishedListener(new LoadingFeedItemView.OnLoadingFinishedListener() {
+            @Override
+            public void onLoadingFinished() {
+                showLoadingView = false;
+                notifyItemChanged(0);
+            }
+        });
+        holder.loadingFeedItemView.startLoading();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (showLoadingView && position == 0) {
+            return VIEW_TYPE_LOADER;
+        } else {
+            return VIEW_TYPE_DEFAULT;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return feedItems.size();
+    }
+
+    Long last_time = 0L;
+
+    public void updateItems(boolean animated) {
+        Cursor cursor;
+
+        do {
+            cursor = chatDBlocal.rawQuery(
+                    "SELECT * FROM chat WHERE data >" + last_time.toString() + " ORDER BY data", null);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (!cursor.moveToFirst());
+
+        cursor.moveToLast();
+        last_time = cursor.getLong(cursor
+                .getColumnIndex("data"));
+
+        HashMap<String, Object> hm;
+
+        cursor.moveToFirst();
+        do {
+            hm = new HashMap<>();
+            hm.put("author", cursor.getString(cursor.getColumnIndex("author")));
+            hm.put("img", cursor.getString(cursor.getColumnIndex("img")));
+            hm.put("text", cursor.getString(cursor.getColumnIndex("text")));
+            hm.put("list_author_time", new SimpleDateFormat(
+                    "HH:mm - dd.MM.yyyy").format(new Date(cursor
+                    .getLong(cursor.getColumnIndex("data")))));
+            feedItems.add(0, new FeedItem(12, false, hm.get("img").toString(), hm.get("author").toString(), hm.get("text").toString()));
+        } while (cursor.moveToNext());
+        if (animated) {
+            notifyItemRangeInserted(0, feedItems.size());
+        } else {
+            notifyDataSetChanged();
+        }
+    }
+
+    public void checkNewItems(boolean animated) {
+        Cursor cursor = chatDBlocal.rawQuery(
+                "SELECT * FROM chat WHERE data >" + last_time.toString() + " ORDER BY data", null);
+        if (cursor.moveToFirst()) {
+            HashMap<String, Object> hm;
+            do {
+                hm = new HashMap<>();
+                hm.put("author", cursor.getString(cursor.getColumnIndex("author")));
+                hm.put("img", cursor.getString(cursor.getColumnIndex("img")));
+                hm.put("text", cursor.getString(cursor.getColumnIndex("text")));
+                hm.put("list_author_time", new SimpleDateFormat(
+                        "HH:mm - dd.MM.yyyy").format(new Date(cursor
+                        .getLong(cursor.getColumnIndex("data")))));
+                feedItems.add(0, new FeedItem(12, false, hm.get("img").toString(), hm.get("author").toString(), hm.get("text").toString()));
+            } while (cursor.moveToNext());
+        }
+        if (animated) {
+            notifyItemRangeInserted(0, feedItems.size());
+        } else {
+            notifyDataSetChanged();
+        }
+    }
+
+    public void setOnFeedItemClickListener(OnFeedItemClickListener onFeedItemClickListener) {
+        this.onFeedItemClickListener = onFeedItemClickListener;
+    }
+
+    public void showLoadingView() {
+        showLoadingView = true;
+        notifyItemChanged(0);
+    }
+
+    public static class CellFeedViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivFeedCenter)
+        ImageView ivFeedCenter;
+        @BindView(R.id.tvFeedBottom)
+        TextView tvFeedBottom;
+        @BindView(R.id.btnComments)
+        ImageButton btnComments;
+        @BindView(R.id.btnLike)
+        ImageButton btnLike;
+        @BindView(R.id.btnMore)
+        ImageButton btnMore;
+        @BindView(R.id.vBgLike)
+        View vBgLike;
+        @BindView(R.id.ivLike)
+        ImageView ivLike;
+        @BindView(R.id.tsLikesCounter)
+        TextSwitcher tsLikesCounter;
+        @BindView(R.id.ivUserProfile)
+        ImageView ivUserProfile;
+        @BindView(R.id.tvAuthor)
+        TextView tvAuthor;
+        @BindView(R.id.vImageRoot)
+        FrameLayout vImageRoot;
+
+        FeedItem feedItem;
+
+        public CellFeedViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+        private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+            ImageView bmImage;
+
+            public DownloadImageTask(ImageView bmImage) {
+                this.bmImage = bmImage;
+            }
+
+            protected Bitmap doInBackground(String... urls) {
+                String urldisplay = urls[0];
+                Bitmap mIcon11 = null;
+                try {
+                    InputStream in = new java.net.URL(urldisplay).openStream();
+                    mIcon11 = BitmapFactory.decodeStream(in);
+                } catch (Exception e) {
+                    Log.e("Error", e.getMessage());
+                    e.printStackTrace();
+                }
+                return mIcon11;
+            }
+
+            protected void onPostExecute(Bitmap result) {
+                bmImage.setImageBitmap(result);
+            }
+        }
+
+        public void bindView(FeedItem feedItem) {
+            this.feedItem = feedItem;
+            int adapterPosition = getAdapterPosition();
+            new DownloadImageTask(ivFeedCenter).execute("http://u0306965.plsk.regruhosting.ru" + feedItem.path);
+            tvAuthor.setText(feedItem.author);
+            tvFeedBottom.setText(feedItem.text);
+            btnLike.setImageResource(feedItem.isLiked ? R.drawable.ic_heart_red : R.drawable.ic_heart_outline_grey);
+            tsLikesCounter.setCurrentText(vImageRoot.getResources().getQuantityString(
+                    R.plurals.likes_count, feedItem.likesCount, feedItem.likesCount
+            ));
+        }
+
+        public FeedItem getFeedItem() {
+            return feedItem;
+        }
+    }
+
+    public static class LoadingCellFeedViewHolder extends CellFeedViewHolder {
+
+        LoadingFeedItemView loadingFeedItemView;
+
+        public LoadingCellFeedViewHolder(LoadingFeedItemView view) {
+            super(view);
+            this.loadingFeedItemView = view;
+        }
+
+        @Override
+        public void bindView(FeedItem feedItem) {
+            super.bindView(feedItem);
+        }
+    }
+
+    public static class FeedItem {
+        public int likesCount;
+        public boolean isLiked;
+        public String path;
+        public String author;
+        public String text;
+
+        public FeedItem(int likesCount, boolean isLiked, String path, String author, String text) {
+            this.likesCount = likesCount;
+            this.isLiked = isLiked;
+            this.path = path;
+            this.author = author;
+            this.text = text;
+        }
+    }
+
+    public interface OnFeedItemClickListener {
+        void onCommentsClick(View v, int position);
+
+        void onMoreClick(View v, int position);
+
+        void onProfileClick(View v);
+    }
+}
